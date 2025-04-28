3086cd6a2a2acc080d9ac438928fc7ba0a9c3c80
chp_h <veron.grigoreva@gmail.com>
2025-04-28 09:32:29 +0300

    began to write getOption - right version of commit

--- Animal_Nursery_Java/controller/AnimalController.java
+++ Animal_Nursery_Java/controller/AnimalController.java
@@ -2,6 +2,7 @@ package controller;
 
 import java.io.IOException;
 import java.util.InputMismatchException;
+import java.util.Scanner;
 import java.util.logging.FileHandler;
 import java.util.logging.Level;
 import java.util.logging.SimpleFormatter;
@@ -10,29 +11,48 @@ import java.util.logging.Logger;
 
 public class AnimalController {
 
-    public void run() {
-        Logger logger = Logger.getLogger(AnimalController.class.getName());
+    Scanner scanner = new Scanner(System.in);
 
-        try {
-            FileHandler fileHandler = new FileHandler("log.txt", true);
-            logger.addHandler(fileHandler);
-            SimpleFormatter sFormat = new SimpleFormatter();// классический текстовый формат
-            fileHandler.setFormatter(sFormat);
-            try {
-                
-                    }
-                } else {
-                    System.out.println("");
-                    logger.log(Level.WARNING, "");
-                }
-            } catch (InputMismatchException e) {
-                logger.log(Level.WARNING, "");
-            } catch (Exception e) {
-                logger.log(Level.WARNING, e.getMessage());
-            }
-        } catch (IOException e) {
-            logger.warning("Ошибка записи в файл ");
+    public int getOption(){
+
+        try{
+
+            int choice = scanner.nextInt();
+
+            return choice;
+        }catch{
+
         }
+
+        
     }
 
+
 }
+
+//     public void run() {
+//         Logger logger = Logger.getLogger(AnimalController.class.getName());
+
+//         try {
+//             FileHandler fileHandler = new FileHandler("log.txt", true);
+//             logger.addHandler(fileHandler);
+//             SimpleFormatter sFormat = new SimpleFormatter();// классический текстовый формат
+//             fileHandler.setFormatter(sFormat);
+//             try {
+                
+//                     }
+//                 } else {
+//                     System.out.println("");
+//                     logger.log(Level.WARNING, "");
+//                 }
+//             } catch (InputMismatchException e) {
+//                 logger.log(Level.WARNING, "");
+//             } catch (Exception e) {
+//                 logger.log(Level.WARNING, e.getMessage());
+//             }
+//         } catch (IOException e) {
+//             logger.warning("Ошибка записи в файл ");
+//         }
+//     }
+
+// }
